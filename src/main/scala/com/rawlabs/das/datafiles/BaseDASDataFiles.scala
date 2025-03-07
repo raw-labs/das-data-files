37be67a64ecf74e837becc76c25229f1edca6b1b
torcato <cesar.matos@gmail.com>
2025-03-07 16:57:31 +0100

    Adding base class for DAS's

---
+++ src/main/scala/com/rawlabs/das/datafiles/BaseDASDataFiles.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2025 RAW Labs S.A.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0, included in the file
+ * licenses/APL.txt.
+ */
+
+package com.rawlabs.das.datafiles
+
+import org.apache.spark.sql.SparkSession
+
+import com.rawlabs.das.datafiles.table._
+import com.rawlabs.das.sdk.DASSettings
+import com.rawlabs.das.sdk.scala.{DASFunction, DASSdk, DASSdkBuilder, DASTable}
+import com.rawlabs.protocol.das.v1.functions.FunctionDefinition
+import com.rawlabs.protocol.das.v1.tables.TableDefinition
+
+/**
+ * The main plugin class that registers one table per file.
+ */
+abstract class BaseDASDataFiles(options: Map[String, String]) extends DASSdk {
+  protected val dasOptions = new DASDataFilesOptions(options)
+
+  protected lazy val sparkSession: SparkSession = SParkSessionBuilder.build("dasDataFilesApp", dasOptions)
+
+  protected val hppFileCache: HttpFileCache = HttpFileCache.build(dasOptions.httpOptions)
+
+  // Build a list of our tables
+  def tables: Map[String, BaseDataFileTable]
+
+  // Return the definitions to the engine
+  override def tableDefinitions: Seq[TableDefinition] = tables.values.map(_.tableDefinition).toSeq
+
+  // This plugin has no custom functions
+  override def functionDefinitions: Seq[FunctionDefinition] = Seq.empty
+
+  /**
+   * Return the table instance for the requested name (if found).
+   */
+  override def getTable(name: String): Option[DASTable] = tables.get(name)
+
+  /**
+   * No custom functions
+   */
+  override def getFunction(name: String): Option[DASFunction] = None
+
+  override def close(): Unit = {
+    sparkSession.stop()
+    hppFileCache.shutdown()
+  }
+
+}
+
+/**
+ * Builder for the "data-files" DAS type. The engine calls build() with the user-provided config, returning a new
+ * DASHttp instance.
+ */
+class DASDataFilesBuilder extends DASSdkBuilder {
+
+  // This must match your "type" field in the config for the plugin
+  override def dasType: String = "datafiles"
+
+  override def build(options: Map[String, String])(implicit settings: DASSettings): DASSdk = {
+    new DASDataFiles(options)
+  }
+}
--- src/main/scala/com/rawlabs/das/datafiles/DASDataFiles.scala
+++ src/main/scala/com/rawlabs/das/datafiles/DASDataFiles.scala
@@ -12,37 +12,17 @@
 
 package com.rawlabs.das.datafiles
 
-import scala.jdk.CollectionConverters.CollectionHasAsScala
-
-import org.apache.spark.sql.SparkSession
-
-import com.rawlabs.das.sdk.DASSdkInvalidArgumentException
-import com.rawlabs.das.sdk.scala.{DASFunction, DASSdk, DASTable}
-import com.rawlabs.protocol.das.v1.functions.FunctionDefinition
-import com.rawlabs.protocol.das.v1.tables.TableDefinition
-import com.typesafe.config.{Config, ConfigFactory}
-
-object DASDataFiles {
-  private val config: Config = ConfigFactory.load()
-  private val cacheDirStr = config.getString("raw.das.data-files.cache-dir")
-  private val idleTimeoutMillis = config.getLong("raw.das.data-files.cache-idle-timeout-ms")
-  private val evictionCheckMillis = config.getLong("raw.das.data-files.cache-eviction-check-ms")
-
-}
+import com.rawlabs.das.datafiles.table._
+import com.rawlabs.das.sdk.scala.{DASSdk, DASSdkBuilder}
+import com.rawlabs.das.sdk.{DASSdkInvalidArgumentException, DASSettings}
 
 /**
  * The main plugin class that registers one table per file.
  */
-class DASDataFiles(options: Map[String, String]) extends DASSdk {
-  import DASDataFiles._
-  private val dasOptions = new DASDataFilesOptions(options)
+class DASDataFiles(options: Map[String, String]) extends BaseDASDataFiles(options) {
 
-  private lazy val sparkSession = SParkSessionBuilder.build("dasDataFilesApp", dasOptions)
-
-  private val hppFileCache =
-    new HttpFileCache(cacheDirStr, idleTimeoutMillis, evictionCheckMillis, dasOptions.httpOptions)
   // Build a list of our tables
-  private val tables: Map[String, BaseDataFileTable] = dasOptions.tableConfigs.map { config =>
+  val tables: Map[String, BaseDataFileTable] = dasOptions.tableConfigs.map { config =>
     val format = config.format.getOrElse(
       throw new DASSdkInvalidArgumentException(s"format not specified for table ${config.name}"))
     format match {
@@ -53,26 +33,18 @@ class DASDataFiles(options: Map[String, String]) extends DASSdk {
       case other     => throw new DASSdkInvalidArgumentException(s"Unsupported format $other")
     }
   }.toMap
+}
 
-  // Return the definitions to the engine
-  override def tableDefinitions: Seq[TableDefinition] = tables.values.map(_.tableDefinition).toSeq
+/**
+ * Builder for the "data-files" DAS type. The engine calls build() with the user-provided config, returning a new
+ * DASHttp instance.
+ */
+class DASDataFilesBuilder extends DASSdkBuilder {
 
-  // This plugin has no custom functions
-  override def functionDefinitions: Seq[FunctionDefinition] = Seq.empty
+  // This must match your "type" field in the config for the plugin
+  override def dasType: String = "datafiles"
 
-  /**
-   * Return the table instance for the requested name (if found).
-   */
-  override def getTable(name: String): Option[DASTable] = tables.get(name)
-
-  /**
-   * No custom functions
-   */
-  override def getFunction(name: String): Option[DASFunction] = None
-
-  override def close(): Unit = {
-    sparkSession.stop()
-    hppFileCache.shutdown()
+  override def build(options: Map[String, String])(implicit settings: DASSettings): DASSdk = {
+    new DASDataFiles(options)
   }
-
 }
